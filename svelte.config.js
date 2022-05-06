e906e61a4bbabc19d6f3ded334ca8b3879804a4e
bartoszstepien01 <bartoszstepien01@interia.pl>
2022-05-06 09:46:05 +0200

    Fix path base in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -13,8 +15,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
-		})
+			fallback: 'index.html',
+		}),
+		paths: {
+			base: dev ? '' : '/quizr'
+		}
 	}
 };
 
